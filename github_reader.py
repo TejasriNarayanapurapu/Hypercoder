5dcaa3a61ec54fd318c2be7ec4e4dcd784b498b1
Tejasri Narayanapurapu <naryanapurapusiva@gmail.com>
2025-07-30 19:41:50 +0530

    Update github_reader.py

--- github_reader.py
+++ github_reader.py
@@ -1,15 +1,10 @@
 import requests
 
-def get_github_issue(owner, repo, issue_number, github_token=None):
+def get_github_issue(owner, repo, issue_number):
     url = f"https://api.github.com/repos/{owner}/{repo}/issues/{issue_number}"
-    headers = {}
-    if github_token:
-        headers["Authorization"] = f"token {github_token}"
-
-    response = requests.get(url, headers=headers)
+    response = requests.get(url)
 
     if response.status_code == 200:
         return response.json()
     else:
-        print(f"Failed to fetch issue. Status code: {response.status_code}")
         return None
