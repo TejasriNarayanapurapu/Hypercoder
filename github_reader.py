a7cd01d6c6a9553bdef5a247d4635ccfacd39279
Tejasri Narayanapurapu <naryanapurapusiva@gmail.com>
2025-07-30 19:44:55 +0530

    Update github_reader.py

--- github_reader.py
+++ github_reader.py
@@ -2,9 +2,11 @@ import requests
 
 def get_github_issue(owner, repo, issue_number):
     url = f"https://api.github.com/repos/{owner}/{repo}/issues/{issue_number}"
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        return response.json()
-    else:
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return None
+    except Exception as e:
         return None
